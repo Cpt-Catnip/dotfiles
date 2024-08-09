1534c6234d8752b24b299c4f0bca633709c2d9e5
Michael Koshakow <mzkosh@gmail.com>
2024-08-09 16:56:03 -0400

    adds wezterm config

---
+++ tilde/.wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+-- theme {{
+config.color_scheme = 'AdventureTime'
+config.font = wezterm.font 'GohuFont 14 Nerd Font Mono'
+config.font_size = 16
+-- }}
+
+return config
+

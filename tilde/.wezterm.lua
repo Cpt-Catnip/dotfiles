4b96b93b5281134ca62b1350b125aa70837e9cf9
Michael Koshakow <mzkosh@gmail.com>
2024-08-09 17:30:53 -0400

    smaller wez font size

--- tilde/.wezterm.lua
+++ tilde/.wezterm.lua
@@ -1,11 +1,10 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- theme {{
-config.color_scheme = 'AdventureTime'
-config.font = wezterm.font 'GohuFont 14 Nerd Font Mono'
-config.font_size = 16
+config.color_scheme = "AdventureTime"
+config.font = wezterm.font("GohuFont 14 Nerd Font Mono")
+config.font_size = 14
 -- }}
 
 return config
-

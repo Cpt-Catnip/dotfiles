67317b9ab26c2b7783c9112ed8933e7b4122c88b
Michael Koshakow <mzkosh@gmail.com>
2024-08-12 11:33:15 -0400

    updates term theme

--- tilde/.wezterm.lua
+++ tilde/.wezterm.lua
@@ -3,8 +3,8 @@ local config = wezterm.config_builder()
 
 -- theme {{
 config.color_scheme = "AdventureTime"
-config.font = wezterm.font("GohuFont 14 Nerd Font Mono")
-config.font_size = 14
+config.font = wezterm.font("GohuFont uni14 Nerd Font Mono")
+config.font_size = 16
 -- }}
 
 return config

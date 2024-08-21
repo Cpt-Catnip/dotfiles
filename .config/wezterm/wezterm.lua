171e80304b16627f74d4a4fa0ecc5bd5f29ae7f8
Michael Koshakow <mkoshakow@etsy.com>
2024-08-21 10:31:14 -0400

    update wezterm theme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- theme {{
-config.color_scheme = "AdventureTime"
+config.color_scheme = "Gruvbox dark, medium (base16)"
 config.font = wezterm.font("GohuFont uni14 Nerd Font Mono")
 config.font_size = 16
 

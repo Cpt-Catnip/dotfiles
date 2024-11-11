48f769d9e69aa2e730a658797f1ad02ba3638d57
Michael Koshakow <mkoshakow@etsy.com>
2024-11-11 08:10:39 -0500

    reenables mode prompt when not in vscode

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -13,7 +13,9 @@ vim.opt.relativenumber = true
 vim.opt.mouse = 'a'
 
 -- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
+if not vim.g.vscode then
+  vim.opt.showmode = false
+end
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
